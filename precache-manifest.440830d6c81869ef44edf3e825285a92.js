330916221787c1810655c9fefb4dc9e7b30bec6f
ilya.shvts <ilya.nl51018@gmail.com>
2020-08-21 16:34:07 +0300

    Updates

---
+++ precache-manifest.440830d6c81869ef44edf3e825285a92.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e1c6fb1a3d7758ce274f5961531c1e",
+    "url": "/github.io/your-current-weather/index.html"
+  },
+  {
+    "revision": "d868d5afbd3840f48596",
+    "url": "/github.io/your-current-weather/static/css/2.4be015c5.chunk.css"
+  },
+  {
+    "revision": "ba2e36925e69ae89b6ed",
+    "url": "/github.io/your-current-weather/static/css/main.af851d0d.chunk.css"
+  },
+  {
+    "revision": "d868d5afbd3840f48596",
+    "url": "/github.io/your-current-weather/static/js/2.47131c23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/your-current-weather/static/js/2.47131c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2e36925e69ae89b6ed",
+    "url": "/github.io/your-current-weather/static/js/main.74d9b52e.chunk.js"
+  },
+  {
+    "revision": "e9d6976613de255eead4",
+    "url": "/github.io/your-current-weather/static/js/runtime-main.a4992e4b.js"
+  },
+  {
+    "revision": "2cbba09ee97727e25522fed0904295c5",
+    "url": "/github.io/your-current-weather/static/media/bg.2cbba09e.jpg"
+  },
+  {
+    "revision": "fbc9f7f5305a322a32ae6e2adfa6f1d3",
+    "url": "/github.io/your-current-weather/static/media/cloudy-day.fbc9f7f5.svg"
+  },
+  {
+    "revision": "f2998250fed6caee02404b565c87c6bd",
+    "url": "/github.io/your-current-weather/static/media/rain.f2998250.svg"
+  },
+  {
+    "revision": "eacc3127180a0e23f50b1460d600d342",
+    "url": "/github.io/your-current-weather/static/media/sun.eacc3127.svg"
+  },
+  {
+    "revision": "1bc312cc5b3e9a68cfcca2e606d4cdcf",
+    "url": "/github.io/your-current-weather/static/media/sunrise.1bc312cc.svg"
+  },
+  {
+    "revision": "db4f302ed2856b49aaf9ff8c298c4afc",
+    "url": "/github.io/your-current-weather/static/media/sunset.db4f302e.svg"
+  }
+]);
